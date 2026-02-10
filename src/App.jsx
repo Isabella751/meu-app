71b7193f107115201117b5b2fd89fd7b3ab65480
Isabella751 <isabella.leite562@gmail.com>
2026-02-10 10:50:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './styles/theme.css'
+import './styles/global.css'
+export function App() {
+
+  return (
+    <>
+      <main>
+      <h1>Olá</h1>
+      <p>insira nome e senha</p>
+      
+        <label>Nome:
+            <input type="email" placeholder="Digite seu nome" required></input>
+        </label>
+      
+        <label>Senha:
+            <input type="password" placeholder="Digite sua senha" required></input>
+        </label>
+      
+        <button type="submit">Enviar</button>
+      </main>
+    </>
+  )
+}
+
+// export default App
